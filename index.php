e71daee6f43af5cff181bac57c7a0e2c4a75599e
dgundi <dgundi@adobe.com>
2016-05-25 09:18:33 +1000

    Add files via upload

--- index.php
+++ index.php
@@ -7,12 +7,15 @@
 require_once('config.php');
 
 /* Check if any URL parameters have been passed */
-  if (!isset($_GET['inputCountry'])) {
-  	require_once('views/search.php');
-  } else {
- 	$country = $_GET['inputCountry'];
+  if (isset($_GET['inputCountry'])) {
+  	$country = $_GET['inputCountry'];
  	$page = $_GET['inputPage'];
  	require_once('controllers/popular.php');
+  }  elseif (isset($_GET['artist'])){
+  	$artist = $_GET['artist'];
+  	require_once('controllers/artistdetails.php');
+  } else {
+  	require_once('views/search.php');
   }
 
  ?>
