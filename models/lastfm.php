cfaea24745254efb685c5a5b230ff8dacc6b1c22
dgundi <dgundi@adobe.com>
2016-05-25 08:51:51 +1000

    Add files via upload

--- models/lastfm.php
+++ models/lastfm.php
@@ -13,18 +13,50 @@ class LastfmApi {
 	private $_apiurl;
 	private $_apikey;
 	private $_country;
+	private $_method;
+	private $_limit;
+	private $_artist;
 	public $_page;
 
-	public function __construct($apiurl,$apikey,$country,$page) {
+
+
+	public function __construct($apiurl,$apikey,$method,$limit) {
 		$this->_apiurl = $apiurl;
 		$this->_apikey = $apikey;
-		$this->_country = $country;
-		$this->_page = $page;
+		$this->_method = $method;
+		$this->_limit = $limit;
 	}
 
-	public function output() {
-		return $this->_apiurl . '&api_key=' . $this->_apikey . '&country=' . $this->_country . '&page=' . $this->_page . '&limit=' . ARTISTS_PER_PAGE;
+	public function output($data) {
+		$params = http_build_query($data);
+		$apiurl = $this->_apiurl . '&' . $params;
+		$apiurl = json_decode(file_get_contents($apiurl),true);
+
+		return $apiurl;
+	}
+
+	public function getArtistbyCountry($country,$page = 1) {
+		$this->_country = $country;
+		$this->_page = $page;
+		$data['api_key'] = $this->_apikey;
+		$data['method'] = $this->_method;
+		$data['limit'] = $this->_limit;
+		$data['country'] = $country;
+		$data['page'] = $page;
+
+		return $this->output($data);
+	}
+
+	public function getArtistTracks($artist) {
+		$this->_artist = $artist;
+		$data['api_key'] = $this->_apikey;
+		$data['method'] = $this->_method;
+		$data['artist'] = $this->_artist;
+
+		return $this->output($data);
 	}
 }
+
+
 
 ?>
